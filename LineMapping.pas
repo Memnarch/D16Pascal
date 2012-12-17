b614d1e5a696e5cc487f5fc8cea72fdd7efd5932
Memnarch <Sebal007@freenet.de>
2012-12-18 00:02:53 +0100

    LineMapping can now read from a line, and the memoryaddress can be set to -1 to signal that its not set

--- LineMapping.pas
+++ LineMapping.pas
@@ -2,34 +2,89 @@ unit LineMapping;
 
 interface
 
+uses
+  Classes, Types;
+
 type
-  TLineMapping = class
+  TLineMapping = class(TPersistent)
   private
-    FMemoryAddress: Word;
+    FMemoryAddress: Integer;
     FASMLine: Integer;
     FD16UnitName: string;
     FElementName: string;
     FUnitLine: Integer;
     function GetText: string;
   public
+    constructor Create();
+    procedure Clear();
+    procedure ReadFromLine(ALine: string);
+    procedure Assign(ASource: TPersistent); override;
     property D16UnitName: string read FD16UnitName write FD16UnitName;
     property ElementName: string read FElementName write FElementName;
     property UnitLine: Integer read FUnitLine write FUnitLine;
     property ASMLine: Integer read FASMLine write FASMLine;
-    property MemoryAddress: Word read FMemoryAddress write FMemoryAddress;
+    property MemoryAddress: Integer read FMemoryAddress write FMemoryAddress;
     property AsText: string read GetText;
   end;
 
 implementation
 
 uses
-  Classes, Types, SysUtils;
+  SysUtils, StrUtils;
 
 { TLineMapping }
 
+procedure TLineMapping.Assign(ASource: TPersistent);
+var
+  LSource: TLineMapping;
+begin
+  if ASource.InheritsFrom(TLineMapping) then
+  begin
+    LSource := TLineMapping(ASource);
+    FD16UnitName := LSource.D16UnitName;
+    FElementName := LSource.ElementName;
+    FUnitLine := LSource.UnitLine;
+    FASMLine := LSource.ASMLine;
+    FMemoryAddress := LSOurce.MemoryAddress;
+  end;
+end;
+
+procedure TLineMapping.Clear;
+begin
+  FD16UnitName := '';
+  FElementName := '';
+  FUnitLine := -1;
+  FASMLine := -1;
+  FMemoryAddress := -1;
+end;
+
+constructor TLineMapping.Create;
+begin
+  Clear();
+end;
+
 function TLineMapping.GetText: string;
 begin
-  Result := D16UnitName + ' ' + ElementName + ' ' + IntToSTr(FUnitLine) + ' ' + IntToSTr(FASMLine) + ' '  + IntToHex(FMemoryAddress, 4);
+  Result := D16UnitName + ',' + ElementName + ',' + IntToStr(FUnitLine) + ',' + IntToStr(FASMLine) + ',$'  + IntToHex(FMemoryAddress, 4);
+end;
+
+procedure TLineMapping.ReadFromLine(ALine: string);
+var
+  LElements: TStringDynArray;
+begin
+  LElements := SplitString(Trim(ALine), ',');
+  if Length(LElements) = 5 then
+  begin
+    D16UnitName := LElements[0];
+    ElementName := LElements[1];
+    UnitLine := StrToInt(LElements[2]);
+    ASMLine := StrToInt(LElements[3]);
+    MemoryAddress := StrToInt(LElements[4]);
+  end
+  else
+  begin
+    raise Exception.Create('Expected 5 elements, but found ' + IntToStr(Length(LElements)) + ' in ' + QuotedStr(ALine));
+  end;
 end;
 
 end.
