b668e47b64bc33391c93bdb4fc83e485caa81832
Memnarch <Sebal007@freenet.de>
2013-01-05 18:59:15 +0100

    Assignments can now handle arrays

--- Assignment.pas
+++ Assignment.pas
@@ -3,10 +3,10 @@ unit Assignment;
 interface
 
 uses
-  Classes, Types, CodeElement, VarDeclaration, WriterIntf;
+  Classes, Types, Generics.Collections, CodeElement, OpElement, VarDeclaration, WriterIntf, DataType;
 
 type
-  TAssignment = class(TCodeElement)
+  TAssignment = class(TOpElement)
   private
     FTargetVar: TVarDeclaration;
     FDereference: Boolean;
@@ -25,12 +25,18 @@ uses
 procedure TAssignment.GetDCPUSource;
 var
   LAccess: string;
+  LParts: TStringDynArray;
 begin
   AWriter.AddMapping(Self);
   SubElements.Items[0].GetDCPUSource(Self);
+  GetModifierAdressOffset(Self);
   if Dereference then
   begin
     Self.Write('set y, [' + FTargetVar.GetAccessIdentifier() + ']');
+    if Modifiers.Count > 0 then
+    begin
+      Self.Write('add y, pop');
+    end;
     Self.Write('set x, pop');
     Self.Write('set [y], x');
   end
@@ -39,11 +45,35 @@ begin
     LAccess := FTargetVar.GetAccessIdentifier;
     if AnsiIndexText(LAccess, ['a', 'b', 'c']) >= 0 then
     begin
+      if Modifiers.Count > 0 then
+      begin
+        Self.Write('set y, pop');
+        Self.Write('add y, ' + LAccess);
+        LAccess := '[y]';
+      end;
       Self.Write('set x, pop');
       Self.Write('set ' + LAccess + ', x');
     end
     else
     begin
+      if Modifiers.Count > 0 then
+      begin
+        Self.Write('set y, pop');
+        if (FTargetVar.IsLocal) and (not (FTargetVar.DataType.RawType = rtString)) then //strings are always given as pointer!
+        begin
+          LParts := SplitString(LAccess, '+');
+          Self.Write('add y, ' + LParts[0]);
+          if Length(LParts) > 1 then
+          begin
+            Self.Write('add y, ' + LParts[1]);
+          end;
+        end
+        else
+        begin
+          Self.Write('add y, [' + LAccess + ']');
+        end;
+        LAccess := 'y';
+      end;
       Self.Write('set x, pop');
       Self.Write('set [' + LAccess + '], x');
     end;
